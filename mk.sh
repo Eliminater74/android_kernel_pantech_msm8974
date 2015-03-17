ff37cae56c15093dbb1be10b549ff11ee2a21eea
Hyunsik <blogcin@naver.com>
2015-03-17 20:46:34 +0900

    Add Script

--- mk.sh
+++ mk.sh
@@ -1,3 +1,22 @@
-make -j16 cyanogenmod_ef56s_defconfig
+#!/bin/sh
 
-make -j16
+echo "/**************************************"
+echo "/**************************************"
+echo "**************************************/"
+echo "\n Secret Note : 1 \n Popup Note : 2 \n LTE A :3 \n"
+
+read -r a
+
+if [ "$a" = 1 ] ; then
+make -j16 cyanogenmod_ef59s_defconfig
+fi
+
+if [ "$a" = 2 ] ; then
+make -j16 cyanogenmod_ef65s_defconfig
+fi
+
+if [ "$a" = 3 ] ; then
+make -j16 cyanogenmod_ef56s_defconfig
+fi
+
+make -j16 
